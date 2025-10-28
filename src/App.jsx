ab65f2fc1c3bb57f3fdeec5645a949122c9df84f
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-10-28 13:19:43 -0700

    setup fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { use, useState } from 'react'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [name, setName] = useState("Bob")
   useEffect(()=>{
-    
+    fetch(`https://api.agify.io?name=${name}`)
   },[name]);
 
   return (
